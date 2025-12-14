d0c62106bb40231959e9724a76629631656932b7
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2025-12-14 18:42:01 +0530

    Time: 2 ms (14.59%), Space: 13.5 MB (30.16%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l=0
+        r=len(nums)-1
+        if target not in nums:
+            return -1
+        while l<r:
+            mid=(l+r)//2
+            
+            if target<=nums[mid]:
+                r=mid
+            else:
+                l=mid+1
+        return l
+        
