88de3bba4521cf6a8127c3de6c632c57ed4968aa
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2025-12-25 21:52:38 +0530

    Time: 6 ms (22.13%), Space: 12.6 MB (62.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s)==1:
+            return False
+        stack=[]
+        hash1={')':'(',']':'[','}':'{'}
+        for ch in s:
+            if ch in hash1:
+                if hash1[ch] in stack and hash1[ch]==stack[-1]:
+                    stack.pop(-1)
+                elif hash1[ch] in stack and hash1[ch] != stack[-1]:
+                    return False
+                elif hash1[ch] not in stack:
+                    return False
+            else:
+                stack.append(ch)
+        if stack==[]:
+            return True
+        return False
+                
+
