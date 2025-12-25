4f04de67cb5dc98abfb0bfe9e56093a196c2ca0f
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2025-12-25 20:59:40 +0530

    Time: 134 ms (22.44%), Space: 19.4 MB (34.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,14 +5,13 @@ class Solution(object):
         :rtype: int
         """
         l=0
-        i=1
-        maxi=0
-        for i in range(len(prices)):
-            if prices[i]-prices[l]<0:
+        max_prof=0
+        for i in range(1,len(prices),1):
+            if prices[i]-prices[l]>=0 and l<i:
+                max_prof=max(max_prof,prices[i]-prices[l])
+            elif prices[i]-prices[l]<0 and l<i:
                 l=i
-                
-            maxi=max(maxi,prices[i]-prices[l])
 
+        return max_prof
 
-        return maxi
-        
+            
