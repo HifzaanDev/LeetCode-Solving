30f28697538d89441484ccb86e9143f74223da48
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2026-01-04 19:06:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,13 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        left=0
-        seen=set()
-        maxl=0
-        for right in range(len(s)):
-            while s[right] in seen:
-                seen.remove(s[left])
-                left=left+1
-            seen.add(s[right])
-            maxl=max(maxl,(right-left)+1)
-        return maxl
+        if s == "":
+            return 0
+        elif len(s)==1:
+            return 1
+        seen=[]
+        l=0
+        answer=0
+        for i in range(0,len(s)):
+            if s[i] in seen:
+                answer=max(answer,i-l)
+                l=i
+                seen.remove(s[i])
+                seen.append(s[i])
+            elif s[i] not in seen:
+                seen.append(s[i])
+        if len(s) == len(set(seen)):
+            return len(s)
+        
+        return answer
+
