7e134830284cc60990aa4e0c894ecf8e03c1f32a
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2026-01-05 20:24:26 +0530

    Time: 399 ms (26.31%), Space: 13.7 MB (12.27%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,3 +1,5 @@
+import math
+
 class Solution(object):
     def minEatingSpeed(self, piles, h):
         """
@@ -5,21 +7,24 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
-        left,right=1,max(piles)
-        def can(k):
-            total=0
+        r=max(piles)
+        l=1
+        k=r
+        while l<=r:
+            mid=(l+r)//2
+            sum_of_hours_per_pile=0
             for p in piles:
-                time=math.ceil(p/float(k))
-                total=total+time
-            if total<=h:
-                return True
-            else:
-                return False
-        while left<right:
-            mid=(left+right)//2
-            if can(mid):
-                    right=mid
-            else:
-                left=mid+1
+                hours=math.ceil(float(p)/mid)
+                sum_of_hours_per_pile=sum_of_hours_per_pile+hours
+            if sum_of_hours_per_pile<=h:
+                k=min(k,mid)
+                r=mid-1
+            elif sum_of_hours_per_pile>h:
+                l=mid+1
+            elif sum_of_hours_per_pile==h:
+                k=min(k,mid)
+                r=mid-1
+                
 
-        return left
+    
+        return k
