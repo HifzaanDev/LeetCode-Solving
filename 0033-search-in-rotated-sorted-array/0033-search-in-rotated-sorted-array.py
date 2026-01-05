e44fa68da0df83c1a26d791c6e5f6f2e232a5ce2
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2026-01-05 23:26:54 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.32%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,14 +3,22 @@ class Solution(object):
         """
         :type nums: List[int]
         :type target: int
-        :rtype: int
+        :rtype: bool
         """
         l = 0
         r = len(nums) - 1
         while l<=r:
+            
             mid=(r+l)//2
+            
             if nums[mid]==target:
                 return mid
+
+            
+            if nums[l]==nums[mid]==nums[r]:
+                l=l+1
+                r=r-1
+                continue
             if nums[l]<=nums[mid]:
                 if nums[l]<=target<nums[mid]:
                     r=mid-1
@@ -22,4 +30,3 @@ class Solution(object):
                 else:
                     r=mid-1
         return -1
-
