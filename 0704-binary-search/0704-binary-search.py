0be23f81fdc9c680589e4aa46ffa160ac4709520
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2026-01-05 23:35:21 +0530

    Time: 1 ms (13.77%), Space: 13.4 MB (65.2%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -7,14 +7,13 @@ class Solution(object):
         """
         l=0
         r=len(nums)-1
+        if target not in nums:
+            return -1
         while l<=r:
-            mid=(l+r)//2
-            if nums[mid]==target:
-                return mid
-            elif target<nums[mid]:
+            mid=(l+r)/2
+            if target<nums[mid]:
                 r=mid-1
-            else:
+            elif target>nums[mid]:
                 l=mid+1
-        
-        return -1
- 
+            elif target==nums[mid]:
+                return mid
