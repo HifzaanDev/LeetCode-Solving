78e2d1f919fa6f85bb00e946a64a77fcdfeed853
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2026-01-09 19:35:32 +0530

    Time: 36 ms (45.28%), Space: 12.8 MB (64.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -8,24 +8,24 @@ class Solution(object):
         hash1={}
         hash2={}
         l=0
+        
         for i in range(len(s1)):
             if s1[i] in hash1:
-                hash1[s1[i]]=hash1[s1[i]]+1
+                hash1[s1[i]]=  hash1[s1[i]]+1
             else:
-                hash1[s1[i]]=1
-
+                  hash1[s1[i]]=1
+        
         for r in range(len(s2)):
             if s2[r] in hash2:
                 hash2[s2[r]]=hash2[s2[r]]+1
             else:
-                hash2[s2[r]]=1     
+                hash2[s2[r]]=1
 
-            if r-l+1>len(s1):
+            while r-l+1>len(s1):
                 hash2[s2[l]]=hash2[s2[l]]-1
-                if  hash2[s2[l]]==0:
-                    del  hash2[s2[l]]
+                if hash2[s2[l]]==0:
+                    del hash2[s2[l]]
                 l=l+1
-
             if r-l+1==len(s1):
                 if hash1==hash2:
                     return True
