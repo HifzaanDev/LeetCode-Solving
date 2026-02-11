27687db833d30d45b81953b76bcd538eac5c5d68
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2026-02-12 00:51:42 +0530

    Time: 101 ms (95.65%), Space: 15.4 MB (74.69%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,31 +5,26 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        hash_count = {}
+        max_freq=0
+        max_len=0
         l=0
-        hash={}
-        repmax=0
-        size=0
-        for r in range (len(s)):
-            
-            if s[r] in hash:
-                hash[s[r]]=hash[s[r]]+1
+
+        #find max char count
+        for i in range(len(s)):
+            if s[i] in hash_count:
+                hash_count[s[i]] = hash_count[s[i]]+1
             else:
-                hash[s[r]]=1
-
-            repmax=max(repmax,hash[s[r]])
-            
-            while r-l+1-repmax>k:
-                hash[s[l]]=hash[s[l]]-1
-                if hash[s[l]]==0:
-                    del hash[s[l]]
+                hash_count[s[i]]=1
+        #updating max frequency accordingly
+            if hash_count[s[i]]>max_freq:
+                max_freq=hash_count[s[i]]
+        #if condition fails move l forward 
+            while i-l+1-max_freq>k:
+                hash_count[s[l]]=hash_count[s[l]]-1
                 l=l+1
-            if r-l+1-repmax<=k:
-                size=max(size,r-l+1)
-                
-        return size
-
-            
-
-                
-                
-                        
+            curr_len=i-l+1
+        #update max_len 
+            if curr_len>max_len:
+                max_len=curr_len
+        return max_len
