80c2b08142ab9666d489f60d27e04a419d4fcff3
Hifzaan <165919983+HifzaanDev@users.noreply.github.com>
2026-02-15 03:09:33 +0530

    Time: 0 ms (100%), Space: 12.6 MB (78.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -7,26 +7,26 @@ class Solution(object):
         """
         l = 0
         r = len(nums) - 1
+        if target not in nums:
+            return -1
         while l<=r:
-            
-            mid=(r+l)//2
-            
+            mid=(l+r)//2
             if nums[mid]==target:
                 return mid
-
-            
-            if nums[l]==nums[mid]==nums[r]:
-                l=l+1
+            if nums[l]==nums[r]==nums[mid]:
                 r=r-1
+                l=l+1
                 continue
             if nums[l]<=nums[mid]:
                 if nums[l]<=target<nums[mid]:
                     r=mid-1
                 else:
                     l=mid+1
-            else:
+            elif nums[l]>nums[mid]:
                 if nums[mid]<target<=nums[r]:
                     l=mid+1
                 else:
                     r=mid-1
-        return -1
+    
+
+                    
